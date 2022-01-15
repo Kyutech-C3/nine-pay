858fb7f7603d06669a8dbac659d2651d5dc17f26
pikake <maha.jasminum@gmail.com>
2022-01-15 19:54:23 +0900

    [add] pages/listの追加

---
+++ src/pages/List.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="wrapper">
+    <h1>冷蔵庫</h1>
+    <h2>136P</h2>
+
+    <table class="tbl">
+      <thead>
+        <tr>
+          <th>id</th>
+          <th>もの</th>
+          <th>コード</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in items" :key="item">
+          <td>{{ index + 1 }}</td>
+          <td>{{ item.items_box.title }}</td>
+          <td>{{ item.items_box.code }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          items_box: {
+            title: 'お茶あああああああああああああああああああああああああああ',
+            code: '34567',
+          },
+        },
+        {
+          items_box: {
+            title: 'お茶',
+            code: '3456723456',
+          },
+        },
+        {
+          items_box: {
+            title: 'お茶',
+            code: '34567',
+          },
+        },
+        {
+          items_box: {
+            title: 'お茶',
+            code: '34567',
+          },
+        },
+        {
+          items_box: {
+            title: 'お茶',
+            code: '34567',
+          },
+        },
+        {
+          items_box: {
+            title: 'お茶',
+            code: '34567',
+          },
+        },
+        {
+          items_box: {
+            title: 'お茶',
+            code: '34567',
+          },
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style>
+.wrapper {
+  padding: 20px;
+  margin-bottom: 120px;
+}
+
+.tbl {
+  width: 100%;
+  border-spacing: 0;
+  background-color: white;
+  max-width: 800px;
+  margin: 0 auto;
+}
+
+table th {
+  border-bottom: solid 2px #ff8e3c;
+  padding: 10px 0;
+}
+
+table td {
+  border-bottom: solid 2px #ddd;
+  text-align: center;
+  padding: 10px 20px;
+}
+</style>
--- src/routes.js
+++ src/routes.js
@@ -1,27 +1,27 @@
 import Index from './pages/Index'
-import About from './pages/About'
 import Login from './pages/Login'
 import BarcodeTest from './pages/BarcodeTest'
+import List from './pages/List'
 
 export const routes = [
   {
     path: '/',
     name: 'index',
-    component: Index
+    component: Index,
   },
   {
-    path: '/about',
-    name: 'about',
-    component: About
+    path: '/list',
+    name: 'list',
+    component: List,
   },
   {
     path: '/login',
     name: 'login',
-    component: Login
+    component: Login,
   },
   {
     path: '/barcode_test',
     name: 'barcode_test',
-    component: BarcodeTest
-  }
+    component: BarcodeTest,
+  },
 ]
