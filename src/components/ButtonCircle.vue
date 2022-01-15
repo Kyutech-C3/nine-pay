0d0b32f0e4af8a449e0735a92dfb4eb3507409d0
Kizuku <kotani.kizuku@gmail.com>
2022-01-15 18:47:02 +0900

    [add] トップページ

---
+++ src/components/ButtonCircle.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="button-wrapper">
+        <a :href="link" class="button">
+                {{text}}
+            </a>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        text: String,
+        link: String
+    }
+}
+</script>
+
+<style scoped>
+.button{
+  display: inline-block;
+  text-decoration: none;
+  background: #fffde8;
+  color: rgb(0, 0, 0);
+  width: 120px;
+  height: 120px;
+  line-height: 120px;
+  border-radius: 50%;
+  text-align: center;
+  overflow: hidden;
+  box-shadow: 0px 0px 0px 5px #fffde8;
+  border: dashed 2px #ffb03f;
+  transition: .4s;
+  box-shadow: 3px 3px 10px silver;
+}
+.button-wrapper{
+    font-size: 35px;
+}
+</style>
