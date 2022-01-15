364f1e01f3c2ddff695adfec75f8f2942ca6d638
kyuuri11013 <kyuuri11013@gmail.com>
2022-01-15 20:27:38 +0900

    [add] firebase cloud messaging feature

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import App from './App.vue'
 import { routes } from './routes'
 import { initializeApp } from 'firebase/app'
+import { getMessaging, getToken } from 'firebase/messaging'
 import authState from './plugins/auth'
 import './registerServiceWorker';
 
@@ -28,6 +29,21 @@ var firebaseConfig = {
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig)
 export { firebaseApp }
+
+const messaging = getMessaging()
+// Get token
+getToken(messaging, { vapidKey: process.env.VUE_APP_PUBLIC_VAPID_KEY })
+	.then((currentToken) => {
+		if (currentToken) {
+			// Send the token to your server and update the UI if necessary
+			console.log(`FCM TOKEN: ${currentToken}`)
+		} else {
+			// Show permission request UI
+			console.log('No registration token available. Request permission to generate one.')
+		}
+	}).catch((err) => {
+		console.log('Unable to get permission to notify.', err)
+	})
 
 Vue.use(authState)
 
